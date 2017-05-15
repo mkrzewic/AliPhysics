eb2498b4451fee132ad922ab8e1293d8e15fb76b
mkrzewic <mikolaj.krzewicki@cern.ch>
2017-05-15 11:41:13 +0200

    Improve an error message

--- PWGPP/TPC/AliPerformanceTPC.cxx
+++ PWGPP/TPC/AliPerformanceTPC.cxx
@@ -271,7 +271,6 @@ void AliPerformanceTPC::Init()
     fAnalysisFolder = CreateFolder("folderTPC","Analysis Resolution Folder");
     if(!fUseSparse) fFolderObj = new TObjArray;
  
-    
   // set pt bins
   Int_t nPtBins = 50;
   Double_t ptMin = 1.e-2, ptMax = 20.;
@@ -782,7 +781,7 @@ void AliPerformanceTPC::Exec(AliMCEvent* const mcEvent, AliVEvent *const vEvent,
   if(!bUseMC && GetTriggerClass()) {
     Bool_t isEventTriggered = vEvent->IsTriggerClassFired(GetTriggerClass());
     if(!isEventTriggered) {
-      printf("ERROR: Could not determine trigger class");
+      printf("ERROR: Could not determine trigger class (requested: %s)\n", GetTriggerClass());
       return;
     }
   }
